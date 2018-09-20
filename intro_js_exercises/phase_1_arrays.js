738e50a7ad8434c9883a03ad615b7f250c6532fc
Matt <voorhees.matthew@gmail.com>
2018-09-20 15:19:16 -0700

    Implement twosum in javascript

--- intro_js_exercises/phase_1_arrays.js
+++ intro_js_exercises/phase_1_arrays.js
@@ -1,15 +1,25 @@
+array = [-3,1,1,-5,2,3,5];
+
 Array.prototype.uniq = function() {
   let result = [];
-  let i = 0;
   for (let i = 0; i < this.length; i++) {
-    if (!result.includes(this[i])) {
-    result.push(this[i]);}
+      if (!result.includes(this[i])) {
+      result.push(this[i]);
+    }
   }
   return result;
 };
-// 
-// let array = new Array;
-array = [1,1,2,3,5];
-// 
-// console.log(array.uniq);
 
+Array.prototype.twosum = function() {
+  let result = [];
+  for (let j = 1; j < this.length; j++) {
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] + this[j] === 0) {
+        result.push([i,j]);
+      }
+    }
+  }
+  return result;
+};
+
+console.log(array.twosum());
